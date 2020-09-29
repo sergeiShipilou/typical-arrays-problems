0a2227ab196e4a9f3ab94aef47c292bac9a97486
Sergei Shipilov <sergeishipilou@yandex.by>
2020-09-29 20:49:33 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  return Math.min.apply(null, array);
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  return (array.reduce((a,b) => a + b, 0)) / array.length;
 }
